8f0e51ea95ffa0699693e761b95ef6304092df09
krava101 <kravalex2004@gmail.com>
2023-11-23 13:12:21 +0200

    Tasks 1-5

---
+++ js/task-1-5.js
@@ -0,0 +1,73 @@
+//сума непарних чисел
+// function calculateEvenTotal(number)
+// {
+//   let sum = 0;
+//   for (let i = 1; i <= number; i+=3){
+//       sum +=i;
+//   }
+//   return sum;
+// }
+
+// console.log(calculateEvenTotal(795864));
+
+//////////////////////////////////////////////////////////////////////////////
+
+// ПЕРШЕ ЧИСЛО, ЯКЕ ДІЛИТЬСЯ НА 5 БЕЗ ОСТАЧІ
+// const start = 6;
+// const end = 17;
+// let number;
+// for(let i = start; end; i++ ){
+//   if(i % 5 === 0){
+//     number = i;
+//     break;
+//   }
+// }
+// console.log(number)
+
+
+// console.log(clients.indexOf("Pol")); // 2
+// console.log(clients.indexOf("Monkong")); // -1
+
+/////////////////////////////////////////////////////////////////////////////////
+
+// ПОШУК
+// const clients = ["Mango", "Ajax", "Poly", "Kiwi", "Pody"];
+
+// function search(searchWord) {
+//     for (let i = 0; i < clients.length; i++) {
+//         newString = String(clients[i]).toLowerCase();
+//         if (newString.includes(searchWord.toLowerCase())) {
+//             result = clients[i];
+//             break;
+//         }
+//     }
+//     return result;
+// }
+
+// console.log(search("MaN"));
+
+//////////////////////////////////////////////////////////////////////////////////
+
+// function createArrayOfNumbers(min, max) {
+//     const array = [];
+//   for(let i = min; i <= max; i++){
+//     array.push(i);
+//   }
+//   console.log(array)
+// }
+
+// createArrayOfNumbers(1, 3)
+
+///////////////////////////////////////////////////////////////////////////////////
+
+// function calculateTotalPrice(order) {
+//   let sum = 0;
+//   for (i = 0; i < order.length; i++){
+//     sum += order[i];
+//   }
+//   return sum;
+// }
+
+// console.log(calculateTotalPrice([12, 85, 38, 14]));
+
+////////////////////////////////////////////////////////////////////////////////////
